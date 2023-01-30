e77043a7843f3889ac6f9f253edddc70f15eb408
AhmedAsim-69 <ahmedk082@gmail.com>
2023-01-30 11:34:42 +0500

    All working

--- wzip.c
+++ wzip.c
@@ -1,10 +1,9 @@
+//ALL TESTS PASSED
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void wzip_file(FILE *fr, int check_argc, int *count, char *first_word, char *sec_word);
-int count_lines_in_file(FILE *fp);
-
 
 int main (int argc, char *argv[])
 {   
@@ -25,22 +24,18 @@ int main (int argc, char *argv[])
 }
 
 void wzip_file(FILE *fr, int check_argc, int *count, char *first_word, char *sec_word){
-    // char buffer[512];
     if((fr == NULL) ){
         printf("wzip: file1 [file2 ...]\n");
         exit(1);
     }
-    int c = 0;
     int f = 1;
     while(!feof(fr)){
-        c++;
+        if(feof(fr)){
+            break;
+        }
         if(f == 1){
             *first_word = fgetc(fr);
             f = 0;
-        }
-        if(feof(fr)){
-            // printf("EEEOOOOFFFFF!!!, c = %d", c);
-            break;
         }
         *sec_word = fgetc(fr);
         if((*first_word == *sec_word) ){
@@ -48,17 +43,10 @@ void wzip_file(FILE *fr, int check_argc, int *count, char *first_word, char *sec
                 *first_word = *sec_word;
             }
         else{
-            if(check_argc == 1){
-                // if(*first_word == '\n' && *sec_word == EOF){
-                // break;
-                // }
+            if(check_argc == 1 && *first_word != '\0'){
                 *count = *count + 1;
                 fwrite(count, 4, 1, stdout);
                 fwrite(first_word, 1, 1, stdout);
-                // if(*sec_word == EOF){
-                //     printf("\n------ WHEN WROTE ------\n");
-                //     printf("FIRST = %c, SEC = %c, COUNT = %d\n", *first_word, *sec_word, *count);
-                // }
                 *count = 0;
                 *first_word = *sec_word;
             }
@@ -67,17 +55,4 @@ void wzip_file(FILE *fr, int check_argc, int *count, char *first_word, char *sec
             }
         }
     }
-}
-
-int count_lines_in_file(FILE *fp){
-    int lines = 0;
-    int ch = 0;
-    while(!feof(fp))
-    {
-        ch = fgetc(fp);
-        if (ch == '\n'){
-            lines++;
-        }
-    }
-    return lines;
 }
